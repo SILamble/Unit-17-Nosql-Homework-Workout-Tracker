81f4a7d38d048816f909c0bd3d66bfb521f538a7
stuart lamble <lamble.stuart@googlemail.com>
2020-03-08 17:43:44 +0000

    Updated route in view.js, to route to template stats page

--- routes/view.js
+++ routes/view.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 const path = require("path");
 
-//route to adding a new workout
+//route to HTML page for adding a new exercise
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
+})
+
+// route to HTML page for stats
+router.get("/stats", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
 
 module.exports = router;
